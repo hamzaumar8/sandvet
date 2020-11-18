3e45ad0d0f266a0d20011556e00a91d9d59a8ac8
Hamza Umar <humar6078@gmail.com>
2020-11-18 10:15:01 -0800

    sigp fullname sepearated

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,24 +5,37 @@ from allauth.account.forms import SignupForm
 
 class CustomSignupForm(SignupForm):
     
-    fullname = forms.CharField(
+    first_name = forms.CharField(
         min_length=7,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Full Name',
-            'required': True
+            'placeholder': 'first Name',
+            'required': True,
+            # 'class': 'col-md-6'
         }),
     )
+    Last_name = forms.CharField(
+        min_length=7,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Last Name',
+            'required': True,
+            # 'class': 'col-md-6'
+        }),
+    )
+
 
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
         cleaned_data = super(CustomSignupForm, self).clean()
-        fullname = cleaned_data["fullname"]
-        if ' ' in fullname:
-            print(fullname)
-            user.first_name = fullname.split(' ')[0]
-            user.last_name = fullname.split(' ')[1]
-        else:
-            user.first_fullname = fullname
+        first_name = cleaned_data["first_name"]
+        last_name = cleaned_data["last_name"]
+        # if ' ' in fullname:
+        #     print(fullname)
+        #     user.first_name = fullname.split(' ')[0]
+        #     user.last_name = fullname.split(' ')[1]
+        # else:
+        #     user.first_fullname = fullname
+        user.first_name = first_name
+        user.last_name = last_name
         user.save() 
         return user
 
