d45fed28ce496b0e68fb57073efdcad183b1ca21
Hamza Umar <humar6078@gmail.com>
2021-01-15 06:08:36 +0800

    end of my best

--- sandvet/settings.py
+++ sandvet/settings.py
@@ -205,9 +205,9 @@ if DEBUG:
     EMAIL_HOST_USER = 'server@sandvet.com'
     EMAIL_HOST_PASSWORD = 'p1uhVOKGt}h?'
 
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #     }
-    # }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
